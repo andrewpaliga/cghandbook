ec07b2f318f58bc82bc8d0c34987c63b93c09659
Andrew Paliga <andrewpaliga@MacBook-Air.local>
2023-04-21 10:21:48 -0400

    Some edits adding some gardening content

---
+++ src/components/Hero.jsx
@@ -0,0 +1,59 @@
+import { Fragment } from 'react'
+import Image from 'next/image'
+import clsx from 'clsx'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+
+import { Button } from '@/components/Button'
+import { HeroBackground } from '@/components/HeroBackground'
+import blurCyanImage from '@/images/blur-cyan.png'
+import blurIndigoImage from '@/images/blur-indigo.png'
+
+const codeLanguage = 'javascript'
+const code = `export default {
+  strategy: 'predictive',
+  engine: {
+    cpus: 12,
+    backups: ['./storage/cache.wtf'],
+  },
+}`
+
+const tabs = [
+  { name: 'cache-advance.config.js', isActive: true },
+  { name: 'package.json', isActive: false },
+]
+
+function TrafficLightsIcon(props) {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 42 10" fill="none" {...props}>
+      <circle cx="5" cy="5" r="4.5" />
+      <circle cx="21" cy="5" r="4.5" />
+      <circle cx="37" cy="5" r="4.5" />
+    </svg>
+  )
+}
+
+export function Hero() {
+  return (
+    <div className="overflow-hidden bg-slate-900 dark:-mb-32 dark:mt-[-4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:mt-[-4.75rem] dark:lg:pt-[4.75rem]">
+      <div className="py-16 sm:px-2 lg:relative lg:px-0 lg:py-20">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
+          <div className="relative z-10 md:text-center lg:text-left">
+
+            <div className="relative">
+              <p className="inline bg-clip-text font-display text-5xl tracking-tight text-slate-400">
+                The gardening handbook.
+              </p>
+              <p className="mt-3 text-2xl tracking-tight text-slate-400">
+                 Step-by-step guides for growing everything.
+              </p>
+              <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
+                <Button href="/">Get started</Button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
